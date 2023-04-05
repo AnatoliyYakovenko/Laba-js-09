a6c043032e3a0d1efb5246b10969f00d08e156fc
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-04-05 10:57:31 +0300

    complete third task

--- index.js
+++ index.js
@@ -13,18 +13,18 @@
 // let avg = calculateAverage(numbers);
 // console.log("The average is: " + avg); // має бути 3
 
-2;
-function User(name, age) {
-  this.name = name;
-  this.age = age;
-}
+//2
+// function User(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
 
-User.prototype.sayHello = function () {
-  return `Hi. My name is ${name} and I'm {this.age} years old`;
-};
+// User.prototype.sayHello = function () {
+//   return `Hi. My name is ${this.name} and I'm ${this.age} years old`;
+// };
 
-const user = new User("Sherlock", 27);
-console.log(user.sayHello); // має вивести "Hi. My name is Sherlock and I'm 27 years old"
+// const user = new User("Sherlock", 27);
+// console.log(user.sayHello());
 
 // // 3
 // const c = calculator();
